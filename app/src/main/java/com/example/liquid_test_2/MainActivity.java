b245febad57da1e8a11bdffc51d6ad94b8d85609
prakhar-cs <prakhar.sharma@coinswitch.co>
2019-10-29 16:14:03 +0530

    first

--- app/src/main/java/com/example/liquid_test_2/MainActivity.java
+++ app/src/main/java/com/example/liquid_test_2/MainActivity.java
@@ -2,10 +2,19 @@ package com.example.liquid_test_2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.res.AssetManager;
 import android.os.Bundle;
 
 import org.liquidplayer.javascript.JSContext;
+import org.liquidplayer.javascript.JSFunction;
 import org.liquidplayer.javascript.JSValue;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,12 +23,48 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        JSContext context = new JSContext();
-        JSValue result = context.evaluateScript("(() => { let x = 10; return x; })()");
-        System.out.println("========1");
-        System.out.println(result);
-        System.out.println("========2");
+        runScript(this);
     }
+
+
+    private static String getFromFile(Context androidContextObject, String fileName) throws IOException {
+        AssetManager assetManager = androidContextObject.getAssets();
+        System.out.println(assetManager.list("/"));
+        InputStream is = assetManager.open(fileName);
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader input = new BufferedReader(isr);
+        String line;
+        StringBuilder returnString = new StringBuilder();
+        while ((line = input.readLine())!= null) {
+            returnString.append(line);
+            returnString.append("\n");
+        }
+        return new String(returnString);
+
+    }
+
+    public static String runScript(Context androidContextObject) {
+        try {
+            JSContext context = new JSContext();
+            String myFile = MainActivity.getFromFile(androidContextObject,"android-test-dom.js");
+            System.out.println("==================fileok=======");
+            context.evaluateScript("var window = this");
+            context.evaluateScript(myFile);
+            System.out.println("==================sbbssb=======");
+            System.out.println(context.evaluateScript("window.footest1()"));
+            System.out.println("==================xxxxxxxxxxx=======");
+
+//            context.evaluateScript(myF);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+
+
 
 
 
