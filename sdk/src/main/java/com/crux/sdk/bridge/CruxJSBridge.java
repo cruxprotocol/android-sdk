029cd4ea5526fe6936326ce46f90ca7a71955692
mascot6699 <umangshucool@gmail.com>
2019-11-20 07:10:15 +0000

    extract prepareCruxClientInitConfig method

--- sdk/src/main/java/com/crux/sdk/bridge/CruxJSBridge.java
+++ sdk/src/main/java/com/crux/sdk/bridge/CruxJSBridge.java
@@ -1,32 +1,35 @@
 package com.crux.sdk.bridge;
 
+import java.io.IOException;
 import android.content.Context;
 
-import com.crux.sdk.model.CruxClientInitConfig;
-
-import org.json.JSONObject;
 import org.liquidplayer.javascript.JSContext;
 import org.liquidplayer.javascript.JSFunction;
 import org.liquidplayer.javascript.JSObject;
 import org.liquidplayer.javascript.JSValue;
 
-import java.io.IOException;
+import com.crux.sdk.model.CruxClientInitConfig;
+
 
 public class CruxJSBridge {
     private final JSContext jsContext;
     private final JSObject jsClient;
 
     public CruxJSBridge(CruxClientInitConfig.Builder configBuilder, Context androidContextObject) throws IOException {
-        CruxClientInitConfig cruxClientInitConfig = configBuilder.create();
-        String cruxClientInitConfigString = cruxClientInitConfig.getCruxClientInitConfigString();
         this.jsContext = this.getContextForClient(androidContextObject);
-        System.out.println(jsContext.evaluateScript("cruxClientInitConfig = " + cruxClientInitConfigString + ";"));
-        System.out.println(jsContext.evaluateScript("cruxClientInitConfig['storage'] = inmemStorage;"));
-        System.out.println(jsContext.evaluateScript("cruxClientInitConfig['getEncryptionKey'] = function() { return 'fookey';}"));
+        prepareCruxClientInitConfig(configBuilder);
         System.out.println(jsContext.evaluateScript("cc = new CruxPay.CruxClient(cruxClientInitConfig)"));
         System.out.println(jsContext.evaluateScript("cc.init()"));
         this.jsClient = jsContext.property("cc").toObject();
         // TODO This must be blocking here!
+    }
+
+    private void prepareCruxClientInitConfig(CruxClientInitConfig.Builder configBuilder) {
+        CruxClientInitConfig cruxClientInitConfig = configBuilder.create();
+        String cruxClientInitConfigString = cruxClientInitConfig.getCruxClientInitConfigString();
+        System.out.println(jsContext.evaluateScript("cruxClientInitConfig = " + cruxClientInitConfigString + ";"));
+        System.out.println(jsContext.evaluateScript("cruxClientInitConfig['storage'] = inmemStorage;"));
+        System.out.println(jsContext.evaluateScript("cruxClientInitConfig['getEncryptionKey'] = function() { return 'fookey';}"));
     }
 
     private JSContext getContextForClient(Context androidContextObject) throws IOException {
@@ -65,13 +68,14 @@ public class CruxJSBridge {
         promiseCatch.call(promise, jsFailureHandler);
     }
 
+    public Object JSONtoObject(String jsonString) {
+        JSFunction parse = jsContext.property("JSON").toObject().property("parse").toFunction();
+        return parse.call(null, jsonString).toObject();
+    }
+
 //    public String objectToJSON(JSObject jsObject) {
 //        JSFunction stringify = jsContext.property("JSON").toObject().property("stringify").toFunction();
 //        return stringify.call(null, jsObject).toString();
 //    }
 
-    public Object JSONtoObject(String jsonString) {
-        JSFunction parse = jsContext.property("JSON").toObject().property("parse").toFunction();
-        return parse.call(null, jsonString).toObject();
-    }
 }
