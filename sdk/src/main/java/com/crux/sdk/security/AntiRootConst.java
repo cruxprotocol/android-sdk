9e278b3c410b59e089a3acf361bd089c405b817c
mascot6699 <umangshucool@gmail.com>
2020-01-15 15:57:09 +0000

    Adds basic anti-root added

---
+++ sdk/src/main/java/com/crux/sdk/security/AntiRootConst.java
@@ -0,0 +1,77 @@
+package com.crux.sdk.security;
+
+public final class AntiRootConst {
+
+    public static final String BINARY_SU = "su";
+    public static final String BINARY_BUSYBOX = "busybox";
+    public static final String MAGISK = "magisk";
+
+    private AntiRootConst() throws InstantiationException {
+        throw new InstantiationException("This class is not for instantiation");
+    }
+
+    public static final String[] knownRootAppsPackages = {
+            "com.noshufou.android.su",
+            "com.noshufou.android.su.elite",
+            "eu.chainfire.supersu",
+            "com.koushikdutta.superuser",
+            "com.thirdparty.superuser",
+            "com.yellowes.su",
+            "com.topjohnwu.magisk"
+    };
+
+    public static final String[] knownDangerousAppsPackages = {
+            "com.koushikdutta.rommanager",
+            "com.koushikdutta.rommanager.license",
+            "com.dimonvideo.luckypatcher",
+            "com.chelpus.lackypatch",
+            "com.ramdroid.appquarantine",
+            "com.ramdroid.appquarantinepro",
+            "com.android.vending.billing.InAppBillingService.COIN",
+            "com.chelpus.luckypatcher"
+    };
+
+    public static final String[] knownRootCloakingPackages = {
+            "com.devadvance.rootcloak",
+            "com.devadvance.rootcloakplus",
+            "de.robv.android.xposed.installer",
+            "com.saurik.substrate",
+            "com.zachspong.temprootremovejb",
+            "com.amphoras.hidemyroot",
+            "com.amphoras.hidemyrootadfree",
+            "com.formyhm.hiderootPremium",
+            "com.formyhm.hideroot"
+    };
+
+    // These must end with a /
+    public static final String[] suPaths = {
+            "/data/local/",
+            "/data/local/bin/",
+            "/data/local/xbin/",
+            "/sbin/",
+            "/su/bin/",
+            "/system/bin/",
+            "/system/bin/.ext/",
+            "/system/bin/failsafe/",
+            "/system/sd/xbin/",
+            "/system/usr/we-need-root/",
+            "/system/xbin/",
+            "/cache/",
+            "/data/",
+            "/dev/"
+    };
+
+
+    public static final String[] pathsThatShouldNotBeWritable = {
+            "/system",
+            "/system/bin",
+            "/system/sbin",
+            "/system/xbin",
+            "/vendor/bin",
+            "/sbin",
+            "/etc",
+            //"/sys",
+            //"/proc",
+            //"/dev"
+    };
+}
